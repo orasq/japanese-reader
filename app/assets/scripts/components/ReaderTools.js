a791da9b1f5683562bf26f64918d494093046166
Olivier Rasquin <rasquin.olivier@gmail.com>
2021-01-12 15:04:30 +0100

    modify tools box toggle - useEffect

--- app/assets/scripts/components/ReaderTools.js
+++ app/assets/scripts/components/ReaderTools.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { FaBookmark, FaFont, FaCog } from "react-icons/fa";
 import { RiEdit2Fill } from "react-icons/ri";
 
 function ReaderTools(props) {
+  const tools = useRef(null);
   // states
   const [toolsOpened, setToolsOpened] = useState(false);
 
@@ -11,8 +12,33 @@ function ReaderTools(props) {
     setToolsOpened(!toolsOpened);
   }
 
+  function CloseTools() {
+    setToolsOpened(false);
+  }
+
+  // check if clicked element is tools box
+  // if not, close the dropdown
+  function checkClickOutside(e) {
+    if (e.target !== tools.current && !tools.current.contains(e.target)) {
+      CloseTools();
+    }
+  }
+
+  // effects
+  useEffect(() => {
+    if (toolsOpened) {
+      document.addEventListener("click", checkClickOutside);
+    } else {
+      document.removeEventListener("click", checkClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", checkClickOutside);
+    };
+  }, [toolsOpened]);
+
   return (
-    <div className={`reader-tools ${toolsOpened ? "reader-tools--open" : ""}`}>
+    <div ref={tools} className={`reader-tools ${toolsOpened ? "reader-tools--open" : ""}`}>
       <div className="reader-tools__wrap">
         <RiEdit2Fill className="reader-tools__icons" />
         <FaBookmark className="reader-tools__icons" />
