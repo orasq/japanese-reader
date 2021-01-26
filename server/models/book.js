fc813ea4e7aecacf4e700ef689818d834c7fedbe
Olivier Rasquin <rasquin.olivier@gmail.com>
2021-01-26 16:23:12 +0100

    small edit on mongoose schema

--- server/models/book.js
+++ server/models/book.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: String,
-  cover: String,
+  title: { type: String, maxLength: 50 },
+  cover: {
+    type: String,
+    minLength: 2 /* minLength: 2 to avoid empty strings in state to be accepted */
+  },
   text: String,
   finished: Boolean,
   bookmarkIndex: Number
