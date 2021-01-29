8165e07957368b2354dd9480e6b1ef1b9ad803a4
Olivier Rasquin <rasquin.olivier@gmail.com>
2021-01-29 17:13:33 +0100

    re-change browserrouter basename

--- client/app/assets/scripts/App.js
+++ client/app/assets/scripts/App.js
@@ -36,7 +36,7 @@ function App() {
     <ApolloProvider client={client}>
       <StateContext.Provider value={state}>
         <DispatchContext.Provider value={dispatch}>
-          <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <BrowserRouter basename="/japanese-reader/">
             <FloatingMessage />
             <Header />
             <Switch>
